abaf533d108d4b407ba2ee1c723dc385784c0800
michaelcu <43478115+michaelcu@users.noreply.github.com>
2018-11-17 22:00:58 -0600

    Add files via upload
    Finished:
    Search Algorithm
    Tree Creation Algorithm
    Node Placement Algorithm

--- main.cpp
+++ main.cpp
@@ -11,49 +11,56 @@
 #include <fstream>
 #include <string>
 #include <map>
-#include "morseTree.hpp"
+#include <vector>
+
 using namespace std;
+
+struct node
+{
+	char data;
+	struct node *left = NULL;
+	struct node *right = NULL;
+};
 
 
 
 void placeNode(string s, node * n, char data, int pos) {
 	if (pos >= s.size()) {
 		n->data = data;
-		cout << "Data: "<< n->data << endl;
-		return;
 	}
 
 	if (s[pos] == '.') {
-		n->left = new node;
+		if (n->left == NULL)
+			n->left = new node;
 		placeNode(s, n->left, data, ++pos);
 	}
 
 	else if (s[pos] == '_') {
-		n->right = new node;
+		if (n->right == NULL)
+			n->right = new node;
 		placeNode(s, n->right, data, ++pos);
 	}
+	return;
 }
 
-void createTree(map<char, string> m, node * n) {
+void createTree(map<char, string> & m, node * n) {
 	for (std::map<char, string>::iterator it = m.begin(); it != m.end(); ++it) {
 		placeNode(it->second, n, it->first, 0);
 	}
 }
 
 char searchMorse(string s, node * n, int pos) {
-	if (pos >= s.size()) {
-		cout << "This is the data: " << n->data << endl;
+	if (pos >= s.size() || (n->right == NULL & n->left == NULL)) {
 		return n->data;
 	}
 	if (s[pos] == '.') {
-		searchMorse(s, n->left, ++pos);
+		return searchMorse(s, n->left, ++pos);
 	}
 	else if (s[pos] == '_') {
-		searchMorse(s, n->right, ++pos);
+		return searchMorse(s, n->right, ++pos);
 	}
-	else {
+	else
 		return -1;
-	}
 }
 
 int main() {
@@ -68,17 +75,43 @@ int main() {
 	while (morseCode.good()) {
 		morseCode >> letter >> morseStr;
 		morse[tolower(letter)] = morseStr;
-		//std::cout << morse.find(letter)->first << " " << morse.find(letter)->second << endl;
 	}
 	node * root;
 	root = new node;
 	root->data = NULL;
 
-	createTree(morse, root);
-	searchMorse("._", root, 0);
-	//cout << root->left->data;
-	//cout << endl << "Hello " << searchMorse("._", root, 0)<<endl;
+	std::string delimiter = " ";
 
+	string encode = "ass";
+	string s;
+	vector<string> e;
+	vector<string> temp;
+	vector<char> d;
+	while (encode.size() != 0) {
+
+		s = morse[encode[encode.size() - 1]];
+		e.push_back(s);
+		encode.pop_back();
+	}
+	reverse(e.begin(), e.end());
+
+	for (int i = 0; i < e.size(); i++) {
+		cout << e[i] << endl;
+	}
+
+	createTree(morse, root);
+
+	while (e.size() != 0) {
+		d.push_back(searchMorse(string(e[e.size()-1]), root, 0));
+		e.pop_back();
+	}
+
+	reverse(d.begin(), d.end());
+
+	for (int i = 0; i < d.size(); i++) {
+		cout << d[i];
+	}
+	cout << endl;
 
 	system("pause");
 	morseCode.close();
