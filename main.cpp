fe04e55c3da12619a3b89cf1c2155156e941ff02
Christopher Aram Swayne <42748779+c-swa@users.noreply.github.com>
2018-11-19 12:16:11 -0600

    Changes to reading
    Added comments, and changed variable names to be more descriptive.
    Further changes planned:
    -Fix printing of it words/morse code in reverse order
    -needs more commenting

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //  morseCode
 //
 //  Created by Christopher Swayne on 11/13/18.
-//	Last edited By Michael Cu on 11/17/18.
+//    Last edited By Michael Cu on 11/17/18.
 //  Copyright © 2018 Christopher Swayne. All rights reserved.
 //
 
@@ -12,126 +12,146 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <stack>
 
-using namespace std;
+using std::vector;
+using std::string;
+using std::map;
+using std::ifstream;
+
+using std::cout;
+using std::endl;
+
+using std::stack;
+
 
 struct node
 {
-	char data;
-	struct node *left = NULL;
-	struct node *right = NULL;
+    char data;
+    struct node *left = NULL;
+    struct node *right = NULL;
 };
 
 
 
-void placeNode(string s, node * n, char data, int pos) {
-	if (pos >= s.size()) {
-		n->data = data;
-	}
-
-	if (s[pos] == '.') {
-		if (n->left == NULL)
-			n->left = new node;
-		placeNode(s, n->left, data, ++pos);
-	}
-
-	else if (s[pos] == '_') {
-		if (n->right == NULL)
-			n->right = new node;
-		placeNode(s, n->right, data, ++pos);
-	}
-	return;
+void placeNode(string placeString, node * n, char data, int placePosition) {
+    if (placePosition >= placeString.size()) {
+        n->data = data;
+    }
+    
+    if (placeString[placePosition] == '.') {
+        if (n->left == NULL)
+            n->left = new node;
+        placeNode(placeString, n->left, data, ++placePosition);
+    }
+    
+    else if (placeString[placePosition] == '_') {
+        if (n->right == NULL)
+            n->right = new node;
+        placeNode(placeString, n->right, data, ++placePosition);
+    }
+    return;
 }
 
-void createTree(map<char, string> & m, node * n) {
-	for (std::map<char, string>::iterator it = m.begin(); it != m.end(); ++it) {
-		placeNode(it->second, n, it->first, 0);
-	}
+void createTree(std::map<char, string> & m, node * n) {
+    for (std::map<char, string>::iterator it = m.begin(); it != m.end(); ++it) {
+        placeNode(it->second, n, it->first, 0);
+    }
 }
 
-char searchMorse(string s, node * n, int pos) {
-	if ((pos >= s.size()) || (n->right == NULL & n->left == NULL)) {
-		return n->data;
-	}
-	if (s[pos] == '.') {
-		return searchMorse(s, n->left, ++pos);
-	}
-	else if (s[pos] == '_') {
-		return searchMorse(s, n->right, ++pos);
-	}
-	else
-		return -1;
+char searchMorse(string searchString, node * n, int searchPosition) {
+    if ((searchPosition >= searchString.size()) || (n->right == NULL & n->left == NULL)) {
+        return n->data;
+    }
+    if (searchString[searchPosition] == '.') {
+        return searchMorse(searchString, n->left, ++searchPosition);
+    }
+    else if (searchString[searchPosition] == '_') {
+        return searchMorse(searchString, n->right, ++searchPosition);
+    }
+    else
+        return -1;
 }
 
 int main() {
-	
-	ifstream morseCode;
-	morseCode.open("morse.txt");
-	std::map<char, string> morse;
-	string word = "Hello";
-
-	char letter;
-	string morseStr;
-	while (morseCode.good()) {
-		morseCode >> letter >> morseStr;
-		morse[tolower(letter)] = morseStr;
-	}
-	node * root;
-	root = new node;
-	root->data = NULL;
-
-
-	vector<string> e;
-	vector<string> temp;
-	vector<char> d;
-	std::string encode = "ass hello bass";
-
-
-	std::string delimiter = " ";
-	std::string token;
-	int pos = 0;
-	string s;
-
-	while ((pos = encode.find(delimiter)) != std::string::npos) {
-
-		token = encode.substr(0, pos);
-		std::cout << token << std::endl;
-
-		while (token.length() != 0){
-			s = morse[token[token.size() - 1]];
-			e.push_back(s);
-			token.pop_back();
-		}
-
-		encode.erase(0, pos + delimiter.length());
-	}
-
-	while (encode.length() != 0) {
-		s = morse[encode[encode.size() - 1]];
-		e.push_back(s);
-		encode.pop_back();
-	}
-	std::reverse(e.begin(), e.end());
-
-
-	for (int i = 0; i < e.size(); i++) {
-		std::cout << e[i] << endl;
-	}
-
-	createTree(morse, root);
-
-	while (e.size() != 0) {
-		d.push_back(searchMorse(string(e[e.size()-1]), root, 0));
-		e.pop_back();
-	}
-	std::reverse(d.begin(), d.end());
-
-	for (int i = 0; i < d.size(); i++) {
-		std::cout << d[i];
-	}
-	std::cout << endl;
-
-	getchar();
-	morseCode.close();
+    
+    //Setup input file Stream
+    ifstream morseCode;
+    morseCode.open("morse.txt");
+    if(morseCode.fail()){
+        cout << "failed to open file - 'morse.txt'" << endl;
+        getchar();
+        return 1;
+    }
+    std::map<char, string> morseMap;
+    
+    //Reads in from file to the morseMap
+    char letter;
+    string morseStr;
+    while (morseCode.good()) {
+        morseCode >> letter >> morseStr;
+        morseMap[tolower(letter)] = morseStr;
+    }
+    node * root;
+    root = new node;
+    root->data = NULL;
+    
+    //----------------------------------------------------------
+    //Variables defined
+    //String to encode
+    vector<string> encoder;
+    //temp String to store transitional information
+    vector<string> temp;
+    //Used to decode from morse to 'english'
+    vector<char> decoder;
+    string encodeString = "hello bass";
+    
+    
+    string delimiter = " ";
+    string token;
+    int pushPosition = 0;
+    string pushString;
+    
+    while ((pushPosition = encodeString.find(delimiter)) != std::string::npos) {
+        
+        token = encodeString.substr(0, pushPosition);
+        std::cout << token << std::endl;
+        
+        while (token.length() != 0){
+            pushString = morseMap[token[token.size() - 1]];
+            encoder.push_back(pushString);
+            token.pop_back();
+        }
+        
+        encodeString.erase(0, pushPosition + delimiter.length());
+    }
+    
+    while (encodeString.length() != 0) {
+        pushString = morseMap[encodeString[encodeString.size() - 1]];
+        encoder.push_back(pushString);
+        encodeString.pop_back();
+    }
+    std::reverse(encoder.begin(), encoder.end());
+    
+    //prints out the morse code.
+    for (int i = 0; i < encoder.size(); i++) {
+        std::cout << encoder[i] << endl;
+    }
+    
+    createTree(morseMap, root);
+    
+    while (encoder.size() != 0) {
+        decoder.push_back(searchMorse(string(encoder[encoder.size()-1]), root, 0));
+        encoder.pop_back();
+    }
+    std::reverse(decoder.begin(), decoder.end());
+    
+    for (int i = 0; i < decoder.size(); i++) {
+        std::cout << decoder[i];
+    }
+    std::cout << endl;
+    
+    getchar();
+    morseCode.close();
     return 0;
 }
